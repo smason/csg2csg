6686e90505b39269369b2fd533246fc52bd73841
Sam Mason <sam@samason.uk>
2023-09-05 11:17:54 +0100

    use f-strings to tidy FLUKA cell writing
    everything feels very loose here, shouldn't it be creating a proper
    parse tree rather than just lexing symbols?

--- csg2csg/FLUKACellCard.py
+++ csg2csg/FLUKACellCard.py
@@ -1,54 +1,52 @@
 #!/usr/env/python3
 
-from csg2csg.CellCard import CellCard
-from enum import Enum
 import re
+import warnings
+
+from csg2csg.CellCard import CellCard
+
 
 # turn the generic operation type into a serpent relevant text string
 def fluka_op_from_generic(Operation):
     # if we are not of type operator - we are string do nowt
     if not isinstance(Operation, CellCard.OperationType):
         if Operation == "(":
-            return " +" + Operation + " "
-        elif Operation == ")":
-            return " " + Operation + " "
-        elif int(Operation) < 0:
-            return " +S" + str(abs(int(Operation)))
-        elif int(Operation) > 0:
-            return " -S" + str(int(Operation))
-        else:
-            return Operation
-    else:
-        # otherwise we need to do something
-        if Operation == CellCard.OperationType["NOT"]:
-            string = " -"
-        elif Operation == CellCard.OperationType["AND"]:
-            string = "  "
-        elif Operation == CellCard.OperationType["UNION"]:
-            string = " | "
-        else:
-            string = "unknown operation"
-    # return the operation
-    return string
+            return " + ( "
+        if Operation == ")":
+            return " ) "
+        ival = int(Operation)
+        if ival < 0:
+            return f" +S{abs(ival)}"
+        if ival > 0:
+            return f" -S{ival}"
+        return Operation
+
+    # otherwise we need to do something
+    if Operation == CellCard.OperationType.NOT:
+        return " -"
+    if Operation == CellCard.OperationType.AND:
+        return "  "
+    if Operation == CellCard.OperationType.UNION:
+        return " | "
+
+    # TODO: shouldn't this raise an exception?!
+    warnings.warn(f"Unknown operation {Operation!r}")
+    return "unknown operation"
 
 
 # write the cell card for a fluka cell given a generic cell card
 def write_fluka_cell(filestream, CellCard):
-
-    string = " C" + str(CellCard.cell_id) + " 5 "  # number of adjacent cells
-
-    string += "( "
+    string = f" C{CellCard.cell_id} 5 ( "  # number of adjacent cells
 
     # build the cell description
-    for item in CellCard.cell_interpreted:
-        string += fluka_op_from_generic(item)
+    for op in CellCard.cell_interpreted:
+        string += fluka_op_from_generic(op)
 
-    string += " ) "
-    string += "\n"
+    string += " ) \n"
 
     # removes any multiple spaces
-    string = re.sub("  ", " ", string)
-    string = re.sub("\- \+", " -", string)
+    string = re.sub(" {2,}", " ", string)
+    string = re.sub(r"\- \+", " -", string)
 
     filestream.write(string)
 
